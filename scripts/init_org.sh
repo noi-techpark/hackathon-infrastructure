728104581159e5cde050ca769bdd7fdd39cf937a
Clemens Zagler <c.zagler@noi.bz.it>
2025-07-08 17:54:50 +0200

    working script poc

--- scripts/init_org.sh
+++ scripts/init_org.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 HOST="http://172.17.0.1:3000"
-TOKEN=20ad9db1e6fd7205473fecfdd0d0ae76096c7155
+TOKEN=5c8bfe50db6c1f0be1a1223159ecd8faaaf5c55d
 
 function api {
     curl --silent -X $1 \
@@ -29,72 +29,76 @@ org='{
 }'
 api POST admin/users/hackathon/orgs "$org"
 
-#create users
-username=testuser
-usr=$(jq -n --arg created_at "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")" \
-      --arg email "test2@domain.com" \
-      --arg full_name "Test User" \
-      --arg password "passwordsupersecret" \
-      --arg username "$username" \
-'{
-  "created_at": $created_at,
-  "email": $email,
-  "full_name": $full_name,
-  "must_change_password": true,
-  "password": $password,
-  "restricted": false,
-  "send_notify": false,
-  "source_id": 0,
-  "username": $username,
-  "visibility": "private"
-}')
-api POST admin/users "$usr"
+for teamname in team1 team2 team3; do
+    #create team
+    team=$(jq -n \
+          --arg name "$teamname" \
+          --arg description "Team $teamname" \
+    '{
+      "can_create_org_repo": false,
+      "description": $description,
+      "includes_all_repositories": false,
+      "name": $name,
+      "permission": "write",
+      "units": [
+        "repo.actions",
+        "repo.code",
+        "repo.issues",
+        "repo.ext_issues",
+        "repo.wiki",
+        "repo.ext_wiki",
+        "repo.pulls",
+        "repo.releases",
+        "repo.projects",
+        "repo.ext_wiki"
+      ]
+    }')
+    teamjson=$(api POST orgs/$orgname/teams "$team")
+    teamid=$(echo $teamjson | jq '.id')
+    if [ "$teamid" = "null" ]; then
+        teamid=$(api GET "orgs/2025-summer/teams/search?q=$teamname" | jq '.data[0].id')
+        echo fallback to searched teamid. hope it matches: $teamid
+    fi
 
-#create team
-team=$(jq -n \
-      --arg name "team1" \
-      --arg description "Team 1" \
-'{
-  "can_create_org_repo": false,
-  "description": $description,
-  "includes_all_repositories": false,
-  "name": $name,
-  "permission": "write",
-  "units": [
-    "repo.actions",
-    "repo.code",
-    "repo.issues",
-    "repo.ext_issues",
-    "repo.wiki",
-    "repo.ext_wiki",
-    "repo.pulls",
-    "repo.releases",
-    "repo.projects",
-    "repo.ext_wiki"
-  ]
-}')
-teamjson=$(api POST orgs/$orgname/teams "$team")
-teamid=$(echo $teamjson | jq '.id')
-if [ "$teamid" = "null" ]; then
-    teamid=$(api GET 'orgs/2025-summer/teams/search?q=team1' | jq '.data[0].id')
-    echo fallback to searched teamid. hope it matches: $teamid
-fi
+    # create team repo
+    reponame=$teamname-repo
+    repo='{
+      "auto_init": true,
+      "license": "AGPL-3.0-or-later",
+      "name": "'"$reponame"'",
+      "private": true,
+      "template": false
+    }'
+    api POST orgs/$orgname/repos "$repo"
 
-# add user to team
-api PUT teams/$teamid/members/$username
+    # give repo access to team
+    api PUT teams/$teamid/repos/$orgname/$reponame
 
-# create team repo
-reponame=testrepo
-repo='{
-  "auto_init": true,
-  "license": "AGPL-3.0-or-later",
-  "name": "'"$reponame"'",
-  "private": true,
-  "template": false
-}'
-api POST orgs/$orgname/repos "$repo"
+    for username in user1 user2 user3; do
+        #create users
+        username=$teamname$username
+        usr=$(jq -n --arg created_at "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")" \
+              --arg email "$username@domain.com" \
+              --arg full_name "Test User" \
+              --arg password "passwordsupersecret" \
+              --arg username "$username" \
+        '{
+          "created_at": $created_at,
+          "email": $email,
+          "full_name": $full_name,
+          "must_change_password": true,
+          "password": $password,
+          "restricted": false,
+          "send_notify": false,
+          "source_id": 0,
+          "username": $username,
+          "visibility": "private"
+        }')
+        api POST admin/users "$usr"
 
-# give repo access to team
-api PUT teams/$teamid/repos/$orgname/$reponame
+        # add user to team
+        api PUT teams/$teamid/members/$username
+    done
+done
 
 
