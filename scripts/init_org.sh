82a1afc0cb92c95d946fa7ab9f91acfc0a87aca7
Clemens Zagler <c.zagler@noi.bz.it>
2025-07-08 17:22:30 +0200

    wip setup script

--- scripts/init_org.sh
+++ scripts/init_org.sh
@@ -8,12 +8,12 @@ HOST="http://172.17.0.1:3000"
 TOKEN=20ad9db1e6fd7205473fecfdd0d0ae76096c7155
 
 function api {
-    curl -X POST \
+    curl -X $1 \
         -H "Authorization: token $TOKEN" \
         -H 'accept: application/json' \
         -H 'Content-Type: application/json' \
-        $HOST/api/v1/$1 \
-        -d "$2"
+        $HOST/api/v1/$2 \
+        -d "$3"
 }
 
 # create org
@@ -27,14 +27,15 @@ org='{
     "repo_admin_change_team_access": false
 }'
 
-api admin/users/hackathon/orgs "$org"
+api POST admin/users/hackathon/orgs "$org"
 
 #create users
+username=testuser
 usr=$(jq -n --arg created_at "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")" \
       --arg email "test2@domain.com" \
       --arg full_name "Test User" \
       --arg password "passwordsupersecret" \
-      --arg username "testusername" \
+      --arg username "$username" \
 '{
   "created_at": $created_at,
   "email": $email,
@@ -48,4 +49,40 @@ usr=$(jq -n --arg created_at "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")" \
   "visibility": "private"
 }')
 
-api admin/users "$usr"
+api POST admin/users "$usr"
+
+#create team
+team=$(jq -n \
+      --arg name "team1" \
+      --arg description "Team 1" \
+'{
+  "can_create_org_repo": false,
+  "description": $description,
+  "includes_all_repositories": false,
+  "name": $name,
+  "permission": "write",
+  "units": [
+    "repo.actions",
+    "repo.code",
+    "repo.issues",
+    "repo.ext_issues",
+    "repo.wiki",
+    "repo.ext_wiki",
+    "repo.pulls",
+    "repo.releases",
+    "repo.projects",
+    "repo.ext_wiki"
+  ]
+}')
+
+teamjson=$(api POST orgs/2025-summer/teams "$team")
+teamid=$(echo $teamjson | jq '.id')
+
+# add user to team
+api PUT teams/$teamid/members/$username
+
+# create team repo
+
+# give repo access to team
+
+
